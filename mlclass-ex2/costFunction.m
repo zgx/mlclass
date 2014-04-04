950624928e858e4fe72905591c2e9ddab091c78a
zgx <gxzeng007@gmail.com>
2014-04-04 00:09:34 +0000

    finish 4 of exercises 2

--- mlclass-ex2/costFunction.m
+++ mlclass-ex2/costFunction.m
@@ -20,6 +20,9 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+H = sigmoid(X * theta);
+J = sum(-y .* log(H) - (1 - y) .* log(1 - H)) ./ m;
+grad = sum((H - y) .* X)' ./ m;
 
 
 
