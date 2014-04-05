342c3d90aa292c65ab9bb912c6b3f18c22eba414
zgx <gxzeng007@gmail.com>
2014-04-05 11:36:34 +0000

    完成练习2

--- mlclass-ex2/costFunctionReg.m
+++ mlclass-ex2/costFunctionReg.m
@@ -17,7 +17,13 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+H = sigmoid(X * theta);
+reg = lambda / (2*m) * sum(theta(2:length(theta)) .^ 2);
+J = (1/m) .* sum(-y .* log(H) - (1-y) .* log(1-H)) + reg;
 
+reg_part = (lambda / m) .* theta;
+grad = (X' * (H - y)) ./ m + reg_part;
+grad(1) = grad(1) - reg_part(1);
 
 
 
